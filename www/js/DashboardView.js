f52f5a7ba5a438de04cd5b152689a308272c334d
Avais Khatri <avaiskhatri@hotmail.com>
2016-03-16 01:02:29 +0500

    Update

---
+++ www/js/DashboardView.js
@@ -0,0 +1,7 @@
+var DashBoardView = function(dashboard){
+	
+	
+	
+}
+
+DashBoardView.template = Handlebars.compile($("#dashboard-tpl").html());
---
+++ www/js/main.js
@@ -0,0 +1,46 @@
+var app = {
+	showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
+	showLogin: function(){
+		this.showAlert('Login View Button Triggered','Info');
+	},
+	showDashboard: function(){
+		$('.dynamic-body').html(this.dashboardTpl());
+	},
+	renderHomeView: function() {
+		$('.dynamic-body').html(this.loginTpl());
+	},
+    findByName: function() {
+        console.log('findByName');
+        this.store.findByName($('.search-key').val(), function(employees) {
+            var l = employees.length;
+            var e;
+            $('.employee-list').empty();
+            for (var i=0; i<l; i++) {
+                e = employees[i];
+                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            }
+        });
+    },
+
+    initialize: function() {
+		var self = this;
+		this.store = new MemoryStore(function(){
+			self.renderHomeView();
+		});
+        this.loginTpl = Handlebars.compile($("#login-tpl").html());
+        this.dashboardTpl = Handlebars.compile($("#dashboard-tpl").html());
+		$('#show-dashboard').on('click', $.proxy(this.showDashboard, this));
+		$('#show-login').on('click', $.proxy(this.renderHomeView, this));
+		//$('.search-key').on('keyup', $.proxy(this.findByName, this));
+        //$('.login-view-btn').on('click', $.proxy(this.showLogin, this));
+    }
+
+};
+
+app.initialize();
